d4347b90bee33aac9e6ced950613c42e308ef0bb
Chun Hin D. Tang <dtang955472@gmail.com>
2021-07-07 16:22:51 -0400

    update to my practice

--- app.py
+++ app.py
@@ -76,3 +76,67 @@
 ################################################
 #### Simple Word Replacement Program ####
 
+# sentence = input('Enter your sentence: ')
+# print('Your sentence is: ' + sentence)
+# word1 = input('Enter the word to replace: ')
+# word2 = input('Enter the word to replace it with: ')
+# print(sentence.replace(word1, word2))
+
+################################################
+#### List in Python ####
+
+# # List can mix data types such as str, int, and bool
+# countries = ['USA', "China", 'Italy', 'Japan', "Vietnam", "Korea"]
+# countries2 = list(("France", "Brazil", 'Taiwan'))
+# print(countries) ## ['USA', 'China', 'Italy', 'Japan', 'Vietnam', 'Korea']
+# print(countries2) ## ['France', 'Brazil', 'Taiwan']
+# print(countries[3]) ## Japan
+# print(countries[-1]) ## Korea
+# print(countries[3][2]) ## p
+# print(countries[1:]) ## ['China', 'Italy', 'Japan', 'Vietnam', 'Korea']
+# print(countries[1:4]) ## ['China', 'Italy', 'Japan']
+# print(type(countries)) ## <class 'list'>
+# print(type(countries[3])) ## <class 'str'>
+# print(len(countries)) # len gives the length of a str or list  ## 6
+
+# countries[0] = "UK"
+# print(countries) # ['UK', 'China', 'Italy', 'Japan', 'Vietnam', 'Korea']
+
+################################################
+#### List Methods ####
+
+# list1 = [4, 3, 5, 1, 2]
+# list2 = ['banana', 'apples', 'mango', 'oranges', 'mango']
+# list3 = list2.copy()
+
+# list1.sort()
+# print(list1)
+# list1.extend(list2)  #  add list2 to the end of list1
+# print(list1)
+
+
+# list2.append('cherry') # add cherry to the end of list2
+# print(list2)
+# list2.insert(1, 'grapes') # add grapes at index 1
+# print(list2)
+# list2.remove('banana') # removes banana
+# print(list2)
+# print(list2.index('mango'))
+# print(list2.count('mango'))
+# list2.reverse() # reverse the list
+# print(list2)
+# list2.clear() # clear everything from the list 
+# print(list2)
+
+# print(list3)
+# print(list3.pop()) # prints the values of the last element which is being removed from the llst
+# print(list3) 
+# del list3[0] # deletes an element from the list
+# print(list3)
+# del list3 # deletes the whole list so it no longer exist
+# print(list3)
+
+###################################################
+#### Tuples in Python ####
+
+# Tuples are immutable so it cannot be changes
