54d82d2c7f06735f05c0fee644112e08a05431b3
Chun Hin D. Tang <dtang955472@gmail.com>
2021-07-07 15:05:22 -0400

    practice first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+# print('My name is Chun, my age is', 30)
+# print('Welcome')
+
+###################################
+
+# name = 'Tim'
+# age = 18
+
+# print(name + ' is a boy')
+# print(name + ' is', age)
+# print(name, 'is from turkey')
+# print(name)
+
+######################################
+
+# name = 'Tim'
+
+# #  \n creates line break
+# # print('Hi. \nHow are you')
+
+# # \' adds a ' to the string
+# # \ adds a \ to the string
+# print('Hi. \How are you')
+# print(name.upper())
+# print(name.isupper())
+# print(name.lower())
+# print(name.islower())
+# print(name.lower().islower())
+# print(len(name))
+# print(name.index('i'))
+# print(name.replace('m', 'k'))
+
+###################################
+
+num = 32
+
+print(30)
+print(num)
