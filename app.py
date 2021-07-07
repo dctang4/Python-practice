2a11989a83eacf5a1eaedce8dc40178d0bcbb741
Chun Hin D. Tang <dtang955472@gmail.com>
2021-07-07 15:35:48 -0400

    update to my practice

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+#### Print in Python ####
+
 # print('My name is Chun, my age is', 30)
 # print('Welcome')
 
-###################################
+######################################
+#### Variables in Python ####
 
 # name = 'Tim'
 # age = 18
@@ -12,6 +15,7 @@
 # print(name)
 
 ######################################
+#### Strings in Python ####
 
 # name = 'Tim'
 
@@ -30,9 +34,45 @@
 # print(name.index('i'))
 # print(name.replace('m', 'k'))
 
-###################################
+######################################
+#### Numbers in Python
 
-num = 32
+# num = 32
+# num2 = str(num)
 
-print(30)
-print(num)
+# print(30)
+# print(num)
+# print('number is ' + num2)
+
+# print(30 + 70.2) # addition
+# print(30 - 17.2) # subtraction
+# print(30 / 2) # division
+# print(30 % 7) # remainder after division
+# print(30 * 2) # multiplication
+# print(30 ** 2) # exponents
+
+# print(abs(-16)) # absolute value
+# print(max(4,2,3,12)) # the max value
+# print(min(2,3,1,5)) # the min value
+# print(round(3.5)) # round the number
+# print(bin(5)) # the binary string eg: bin(3) is 0b11
+# print(pow(4,3)) # take 4 to power of 3
+
+# # import math
+# # print(math.sqrt(4))
+
+# from math import *
+# print(sqrt(4))
+
+################################################
+#### User Input ####
+
+# name = input('Input Your Name: ')
+# age = input('Input Your Age: ')
+# ageint = int(age)
+# print('your name is ' + name + ' and your are ' + age + ' years old')
+# print('your name is ' + name + ' and your are', ageint ,'years old')
+
+################################################
+#### Simple Word Replacement Program ####
+
